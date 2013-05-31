62a3cbdc59f86f0733714cb605df177b58c20b4c
James Cartwright <james@perspectivedesign.co.uk>
2013-05-31 09:56:29 +0100

    Added logo to header

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,4 +1,10 @@
-<header class="banner navbar navbar-static-top" role="banner">
+<header class="banner navbar navbar-fixed-top" role="banner">
+  <div class="container">
+  	<div class="row">
+  		<div class="span4 text-center"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white-small.png"></div>
+  		<div class="span8">This is the right header area</div>
+  	</div>
+  </div>
   <div class="navbar-inner">
     <div class="container">
       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
