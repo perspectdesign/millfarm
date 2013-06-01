659911950636c219bb39925cadc4f11073f9aef4
James Cartwright <james@perspectivedesign.co.uk>
2013-06-01 12:17:03 +0100

    Added footer widgets and styling

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,30 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+  <div class="socialstrap">
+	  <div class="container">
+	    <?php dynamic_sidebar('sidebar-footer'); ?>
+	  </div>
+  </div>
+  
+  <div class="footerwidgets">
+	  <div class="container">
+	  	<div class="row">
+	  		<div class="span4">
+	  			<?php dynamic_sidebar('sidebar-bottom-left'); ?>
+	  		</div>
+	  		<div class="span4">
+	  			<?php dynamic_sidebar('sidebar-bottom-middle'); ?>
+	  		</div>
+	  		<div class="span4">
+	  			<?php dynamic_sidebar('sidebar-bottom-right'); ?>
+	  		</div>
+	  	</div>
+	  </div>
+  </div>
+  
+  <div class="bottom-footer">
+  	<div class="container">
+    	<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+	</div>
   </div>
 </footer>
 
