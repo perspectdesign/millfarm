9e6f0d5a3abc454092d096c2b87a11520aa74fd1
James Cartwright <james@perspectivedesign.co.uk>
2013-06-07 20:51:53 +0100

    Modified CSS to adapt responsive layouts

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -4,20 +4,18 @@
   		<div class="span4 text-center"><a href="http://www.millfarmnursery.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white-small.png" alt="Mill Farm Nursery"></a></div>
   		<div class="span8 topnav">
 		  	<div class="navbar-inner">
-			    <div class="container">
-			      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			      </a>
-			      <nav class="nav-main nav-collapse collapse" role="navigation">
-			        <?php
-			          if (has_nav_menu('primary_navigation')) :
-			            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
-			          endif;
-			        ?>
-			      </nav>
-			    </div>
+		      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </a>
+		      <nav class="nav-main nav-collapse collapse" role="navigation">
+		        <?php
+		          if (has_nav_menu('primary_navigation')) :
+		            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
+		          endif;
+		        ?>
+		      </nav>
 			</div>
 		</div>
 	</div>
